b65805c94306bdab2e0445563f7574f6828cefcf
Matt Polito and Travis Anderson <dev+mattpolito+syntaxritual@hashrocket.com>
2013-06-20 13:37:18 -0400

    Add documentation for FormBuilder#label

--- lib/formal/form_builder.rb
+++ lib/formal/form_builder.rb
@@ -1,5 +1,20 @@
 module Formal
+
+  # Public: Adds functionality to rails form builder
+  #
   class FormBuilder < ActionView::Helpers::FormBuilder
+
+    # Public: renders html label element
+    #
+    # method  - String or Symbol object attribute
+    # text    - String text to be rendered inside of `label` markup
+    #           (optional, default: nil)
+    # options - Hash of options
+    #           :error_element - String or Symbol representing html element the
+    #                            error markup will be rendered in.
+    #                            (optional, default: :span)
+    #           :hide_errors   - Boolean to hide error markup if it is to be
+    #                            rendered. (optional)
     def label(method, text = nil, options = {}, &block)
       error_element = options.delete(:error_element) || :span
       hide_errors   = options.delete(:hide_errors)
